19f367eff3fe9f781105820bb360894b3db92360
William Rojas <william.ur@live.com>
2021-08-30 10:55:59 -0600

    added code to run test on loggin with invalid credentials, test passed

--- test/specs/e2e-login.js
+++ test/specs/e2e-login.js
@@ -1,10 +1,24 @@
 describe('E2E Test / logout flow', () => {
     it('Should not login with invalid credentials', () => {
-        // TODO
+        browser.url('http://zero.webappsecurity.com/index.html')
+        $('#signin_button').waitForExist()
+        $('#signin_button').click()
+        $('#login_form').waitForExist()
+        $('#user_login').setValue('invalid')
+        $('#user_password').setValue('invalid')
+        $('input[type="submit"]').click()
+        const error = $('.alert-error')
+        expect(error).toHaveText('Login and/or password are wrong.')
     })
 
     it('Should login with valid credentials', () => {
-        // TODO
+        browser.url('http://zero.webappsecurity.com/index.html')
+        $('#signin_button').waitForExist()
+        $('#signin_button').click()
+        $('#login_form').waitForExist()
+        $('#user_login').setValue('invalid')
+        $('#user_password').setValue('invalid')
+        $('input[type="submit"]').click()
     })
 
     it('Should logout from app', () => {
