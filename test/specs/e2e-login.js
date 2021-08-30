b260071ff6bcb829f2b42dde0e9d5ef781444a96
William Rojas <william.ur@live.com>
2021-08-30 10:37:16 -0600

    updated files under specs and pages to better organize test

---
+++ test/specs/e2e-login.js
@@ -0,0 +1,13 @@
+describe('E2E Test / logout flow', () => {
+    it('Should not login with invalid credentials', () => {
+        // TODO
+    })
+
+    it('Should login with valid credentials', () => {
+        // TODO
+    })
+
+    it('Should logout from app', () => {
+        // TODO
+    })
+})
